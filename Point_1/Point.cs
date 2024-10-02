34d7f67bc68ae8b6219911991fa41c852cf1b366
vladi <vladislavvasilev53@gmail.com>
2024-10-02 21:08:26 +0300

    №1 Point

---
+++ Point_1/Point.cs
@@ -0,0 +1,27 @@
+﻿namespace Point_1;
+
+public class Point
+{
+    public double X { get; set; }
+    public double Y { get; set; }
+    public double Z { get; set; }
+
+    public Point(double x, double y, double z)
+    {
+        X = x;
+        Y = y;
+        Z = z;
+    }
+    public Point(Point pt)
+    {
+        X = pt.X;
+        Y = pt.Y;
+        Z = pt.Z;
+    }
+    public void MoveBy(double dx, double dy, double dz)
+    {
+        X += dx;
+        Y += dy;
+        Z += dz;
+    }
+}
